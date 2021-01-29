a0afa34b487e2f7c1e34fdcad049546c9548ee7a
redu <reduardo19942@gmail.com>
2021-01-29 15:33:44 -0300

    Api iniciada

---
+++ apiLive/src/main/java/com/apiLive/apiLive/ApiLiveApplication.java
@@ -0,0 +1,13 @@
+package com.apiLive.apiLive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiLiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiLiveApplication.class, args);
+	}
+
+}
