a8cec534683daec92554bf7521f0ffe3b1464342
redu <reduardo19942@gmail.com>
2021-01-29 14:21:14 -0300

    models

---
+++ src/app/shared/model/responsePageable.model.ts
@@ -0,0 +1,12 @@
+export class responsePageable{
+    content: any[];
+    first: Boolean;
+    last: Boolean;
+    number: Number;
+    numberOfElements: Number;
+    pageable: any[];
+    size: Number;
+    sort: Number;
+    totalElements: number;
+    totalPages: number;
+}
