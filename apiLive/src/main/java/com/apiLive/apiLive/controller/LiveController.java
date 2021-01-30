769d4e2cd160e65b54354e791f9014829ccaa420
redu <reduardo19942@gmail.com>
2021-01-30 13:53:19 -0300

    Habilitando o crossorigin

--- apiLive/src/main/java/com/apiLive/apiLive/controller/LiveController.java
+++ apiLive/src/main/java/com/apiLive/apiLive/controller/LiveController.java
@@ -6,15 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/lives")
 public class LiveController {
